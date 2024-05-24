c0b23f91be0ae37bc9e38b12530ef8980bb3fa02
Arkady Berenshteyn <a.b.darvin350@gmail.com>
2024-05-24 18:23:08 +0300

    Done

--- src/test/kotlin/cs/hse/bookservice/service/BookServiceIT.kt
+++ src/test/kotlin/cs/hse/bookservice/service/BookServiceIT.kt
@@ -1,14 +1,14 @@
 package cs.hse.bookservice.service
 
 import cs.hse.bookservice.dto.requests.BookRequest
-import cs.hse.bookservice.entity.Book
+import cs.hse.bookservice.dto.requests.UpdateBookRequest
+import cs.hse.bookservice.dto.requests.UpdateRatingRequest
+import jakarta.transaction.Transactional
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import java.lang.IllegalArgumentException
 
 
 @SpringBootTest
@@ -21,24 +21,60 @@ class BookServiceIT {
 
     @Test
     @Order(1)
+    @Transactional
     fun getAllBooks() {
-        Assertions.assertEquals(6, bookService.getAllBooks().size )
+        Assertions.assertEquals(6, bookService.getAllBooks().size)
     }
 
     @Test
     @Order(2)
+    @Transactional
     fun saveNewBook() {
-        val bookInit: BookRequest = BookRequest( "Test", "Auth", "Desc")
-        val bookInit2: BookRequest = BookRequest( "Test", "Auth", "Desc")
+        val bookInit: BookRequest = BookRequest("Test", "Auth", "Desc")
+        val bookInit2: BookRequest = BookRequest("Test", "Auth", "Desc")
 
         Assertions.assertThrows(IllegalArgumentException::class.java) {
             bookService.saveNewBook(bookInit)
             bookService.saveNewBook(bookInit2)
         }
-
-
     }
 
+    @Test
+    @Order(3)
+    @Transactional
+    fun updateBookById_positive() {
+        val request = UpdateBookRequest(1, "NewTest", "Auth", "Desc")
+        val response = bookService.updateBookById(request)
+        Assertions.assertEquals("NewTest", response.title)
+    }
 
+    @Test
+    @Order(4)
+    @Transactional
+    fun updateBookById_negative() {
+        val request = UpdateBookRequest(
+            1,
+            "Understanding Physics: Volume 2",
+            "Isaac Asimov",
+            "Light, Magnetism, and Electricity"
+        )
+        Assertions.assertThrows(IllegalArgumentException::class.java) { bookService.updateBookById(request) }
+    }
 
+    @Test
+    @Order(5)
+    @Transactional
+    fun updateRatingById_positive() {
+        val request = UpdateRatingRequest(1, 4.0)
+        val response = bookService.updateRatingById(request)
+        Assertions.assertEquals(4.0, response.rating)
+    }
+
+    @Test
+    @Order(6)
+    @Transactional
+    fun updateRatingById_negative() {
+        val request = UpdateRatingRequest(1, 3.33)
+        Assertions.assertThrows(IllegalArgumentException::class.java) { bookService.updateRatingById(request) }
+    }
 }
--- src/test/kotlin/cs/hse/bookservice/service/BookServiceTest.kt
+++ src/test/kotlin/cs/hse/bookservice/service/BookServiceTest.kt
@@ -1,6 +1,8 @@
 package cs.hse.bookservice.service
 
 import cs.hse.bookservice.dto.requests.BookRequest
+import cs.hse.bookservice.dto.requests.UpdateBookRequest
+import cs.hse.bookservice.dto.requests.UpdateRatingRequest
 import cs.hse.bookservice.dto.responses.BookResponse
 import cs.hse.bookservice.entity.Book
 import cs.hse.bookservice.repository.BookRepository
@@ -12,7 +14,8 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
-import java.lang.IllegalArgumentException
+import java.util.*
+import kotlin.IllegalArgumentException
 
 class BookServiceTest {
 
@@ -40,12 +43,54 @@ class BookServiceTest {
 
     @Test
     fun saveNewBook() {
-        val bookInit: BookRequest = BookRequest( "Test", "Auth", "Desc")
-        val book: Book = Book( 1,"Test", "Auth", "Desc")
+        val bookInit: BookRequest = BookRequest("Test", "Auth", "Desc")
+        val book: Book = Book(1, "Test", "Auth", "Desc")
         Mockito.`when`(bookRepository.findAll()).thenReturn(mutableListOf(book))
 
-        Assertions.assertThrows(IllegalArgumentException::class.java) { bookService.saveNewBook(bookInit)}
+        Assertions.assertThrows(IllegalArgumentException::class.java) { bookService.saveNewBook(bookInit) }
         Mockito.verify(bookRepository, Mockito.times(0)).save(book)
 
+    }
+
+    @Test
+    fun updateBookById_positive() {
+        val book = Book(1, "Test", "Auth", "Description")
+        Mockito.`when`(bookRepository.findById(1)).thenReturn(Optional.of(book))
+        Mockito.`when`(bookRepository.save(book)).thenReturn(book)
+
+        val request = UpdateBookRequest(1, "NewTest", "Auth", "Desc")
+        val response = bookService.updateBookById(request)
+        Assertions.assertEquals("NewTest", response.title)
+    }
+
+    @Test
+    fun updateBookById_negative() {
+        val book = Book(1, "Test", "Auth", "Description")
+        Mockito.`when`(bookRepository.findById(1)).thenReturn(Optional.of(book))
+        Mockito.`when`(bookRepository.save(book)).thenReturn(book)
+
+        val request = UpdateBookRequest(1, "Test", "Auth", "Description")
+        Assertions.assertThrows(IllegalArgumentException::class.java) { bookService.updateBookById(request) }
+    }
+
+    @Test
+    fun updateRatingById_positive() {
+        val book = Book(1, "Test", "Auth", "Description", 5.0)
+        Mockito.`when`(bookRepository.findById(1)).thenReturn(Optional.of(book))
+        Mockito.`when`(bookRepository.save(book)).thenReturn(book)
+
+        val request = UpdateRatingRequest(1, 4.0)
+        val response = bookService.updateRatingById(request)
+        Assertions.assertEquals(4.0, response.rating)
+    }
+
+    @Test
+    fun updateRatingById_negative() {
+        val book = Book(1, "Test", "Auth", "Description", 5.0)
+        Mockito.`when`(bookRepository.findById(1)).thenReturn(Optional.of(book))
+        Mockito.`when`(bookRepository.save(book)).thenReturn(book)
+
+        val request = UpdateRatingRequest(1, 5.0)
+        Assertions.assertThrows(IllegalArgumentException::class.java) { bookService.updateRatingById(request) }
     }
 }
